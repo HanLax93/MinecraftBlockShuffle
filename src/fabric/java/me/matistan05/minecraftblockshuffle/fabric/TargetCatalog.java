7d674ff1f8b8605861d54947347eb2166b124151
hanlax93 <hanlax93@foxmail.com>
2026-08-08 20:29:00 +0800

    Improve easy-round block selection for better progression

--- src/fabric/java/me/matistan05/minecraftblockshuffle/fabric/TargetCatalog.java
+++ src/fabric/java/me/matistan05/minecraftblockshuffle/fabric/TargetCatalog.java
@@ -23,40 +23,49 @@ final class TargetCatalog {
     private static List<Target> resolved;
 
     static {
-        add(BlockDifficulty.EASY, Dimension.OVERWORLD, "stone", "stone", "cobblestone", "gravel", "sand",
-            "dirt", "grass_block");
-        add(BlockDifficulty.EASY, Dimension.OVERWORLD, "oak", "oak_log", "oak_wood", "stripped_oak_log",
-            "oak_planks", "oak_slab", "oak_stairs", "oak_fence", "oak_fence_gate", "oak_door",
-            "oak_trapdoor", "oak_leaves");
-        add(BlockDifficulty.EASY, Dimension.OVERWORLD, "birch", "birch_log", "birch_wood", "stripped_birch_log",
-            "birch_planks", "birch_slab", "birch_stairs", "birch_fence", "birch_fence_gate", "birch_door",
-            "birch_trapdoor", "birch_leaves");
-        add(BlockDifficulty.EASY, Dimension.OVERWORLD, "craft-basic", "crafting_table", "furnace", "chest",
-            "barrel");
-        add(BlockDifficulty.EASY, Dimension.OVERWORLD, "stone-craft", "stone_slab", "stone_stairs",
-            "cobblestone_slab", "cobblestone_stairs", "cobblestone_wall", "stone_bricks", "stone_brick_slab",
-            "stone_brick_stairs", "stone_brick_wall");
+        // Tier 1 (round starts): terrain-feature oriented targets that should still be reachable
+        // in one round on a normal survival world without forcing immediate trivial farming.
+        add(BlockDifficulty.EASY, Dimension.OVERWORLD, "surface-foundation",
+            "stone", "cobblestone", "tuff", "calcite", "dripstone_block", "deepslate", "cobbled_deepslate",
+            "gravel", "sand", "red_sand", "clay", "mud", "dirt", "coarse_dirt", "grass_block", "moss_block", "podzol",
+            "mycelium", "rooted_dirt",
+            "packed_mud", "snow_block", "ice", "packed_ice", "blue_ice");
+
+        add(BlockDifficulty.EASY, Dimension.OVERWORLD, "forest-heights",
+            "oak_log", "oak_wood", "stripped_oak_log",
+            "birch_log", "birch_wood", "stripped_birch_log",
+            "spruce_log", "spruce_wood", "stripped_spruce_log",
+            "jungle_log", "jungle_wood", "stripped_jungle_wood",
+            "acacia_log", "acacia_wood", "stripped_acacia_log",
+            "dark_oak_log", "dark_oak_wood", "stripped_dark_oak_log",
+            "cherry_log", "cherry_wood", "stripped_cherry_log",
+            "mangrove_log", "mangrove_roots", "muddy_mangrove_roots",
+            "bamboo_block");
+
+        add(BlockDifficulty.EASY, Dimension.OVERWORLD, "feature-crafts",
+            "crafting_table", "furnace", "chest", "barrel",
+            "glass", "glass_pane", "stone_bricks", "chiseled_stone_bricks", "mossy_cobblestone",
+            "mossy_stone_bricks", "terracotta", "white_terracotta");
 
         add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "geology", "granite", "diorite", "andesite",
-            "coarse_dirt", "rooted_dirt", "mud", "clay", "moss_block", "calcite", "tuff",
-            "dripstone_block", "cobbled_deepslate", "polished_andesite", "polished_diorite", "polished_granite");
+            "polished_andesite", "polished_diorite", "polished_granite");
 
-        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "spruce", "spruce_log", "spruce_planks", "spruce_slab",
+        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "spruce", "spruce_planks", "spruce_slab",
             "spruce_stairs", "spruce_fence", "spruce_door", "spruce_trapdoor", "spruce_leaves");
-        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "jungle", "jungle_log", "jungle_planks", "jungle_slab",
+        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "jungle", "jungle_planks", "jungle_slab",
             "jungle_stairs", "jungle_fence", "jungle_door", "jungle_trapdoor", "jungle_leaves");
-        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "acacia", "acacia_log", "acacia_planks", "acacia_slab",
+        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "acacia", "acacia_planks", "acacia_slab",
             "acacia_stairs", "acacia_fence", "acacia_door", "acacia_trapdoor", "acacia_leaves");
-        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "dark_oak", "dark_oak_log", "dark_oak_planks",
+        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "dark_oak", "dark_oak_planks",
             "dark_oak_slab", "dark_oak_stairs", "dark_oak_fence", "dark_oak_door", "dark_oak_trapdoor",
             "dark_oak_leaves");
-        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "cherry", "cherry_log", "cherry_planks", "cherry_slab",
+        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "cherry", "cherry_planks", "cherry_slab",
             "cherry_stairs", "cherry_fence", "cherry_door", "cherry_trapdoor", "cherry_leaves");
-        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "bamboo", "bamboo_block", "bamboo_planks", "bamboo_slab",
+        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "bamboo", "bamboo_planks", "bamboo_slab",
             "bamboo_stairs", "bamboo_mosaic", "bamboo_door", "bamboo_trapdoor");
-        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "mangrove", "mangrove_log", "mangrove_planks",
+        add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "mangrove", "mangrove_planks",
             "mangrove_slab", "mangrove_stairs", "mangrove_fence", "mangrove_door", "mangrove_trapdoor",
-            "mangrove_leaves", "mangrove_roots", "muddy_mangrove_roots");
+            "mangrove_leaves");
         add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "iron", "iron_block", "iron_bars", "iron_door",
             "iron_trapdoor", "cauldron", "hopper");
         add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "copper", "copper_block", "cut_copper", "cut_copper_slab",
@@ -64,8 +73,8 @@ final class TargetCatalog {
         add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "redstone", "redstone_block", "piston",
             "dispenser", "dropper", "observer", "target", "note_block");
         add(BlockDifficulty.NORMAL, Dimension.OVERWORLD, "color", "red_wool", "yellow_wool", "blue_wool",
-            "black_wool", "orange_wool", "white_bed", "red_bed", "yellow_bed", "glass", "glass_pane",
-            "white_wool", "hay_block", "coal_block", "bookshelf");
+            "black_wool", "orange_wool", "white_bed", "red_bed", "yellow_bed", "hay_block", "coal_block",
+            "bookshelf");
 
         add(BlockDifficulty.HARD, Dimension.OVERWORLD, "rare-overworld", "diamond_block", "emerald_block",
             "enchanting_table", "ender_chest", "beacon", "conduit", "obsidian", "crying_obsidian",
@@ -126,6 +135,11 @@ final class TargetCatalog {
 
     private static Set<String> resourcesFor(String family) {
         return switch (family) {
+            case "surface-foundation" -> Set.of("stone", "dirt", "sand", "gravel", "clay", "tuff", "calcite");
+            case "forest-heights" -> Set.of("oak_log", "oak_leaves", "spruce_log", "spruce_leaves", "birch_log", "birch_leaves",
+                "jungle_log", "jungle_leaves", "acacia_log", "acacia_leaves", "dark_oak_log", "dark_oak_leaves",
+                "cherry_log", "cherry_leaves", "mangrove_log", "mangrove_roots");
+            case "feature-crafts" -> Set.of("stone", "clay", "dirt", "cobblestone");
             case "oak" -> Set.of("oak_log", "oak_leaves");
             case "birch" -> Set.of("birch_log", "birch_leaves");
             case "spruce" -> Set.of("spruce_log", "spruce_leaves");
@@ -135,8 +149,7 @@ final class TargetCatalog {
             case "cherry" -> Set.of("cherry_log", "cherry_leaves");
             case "mangrove" -> Set.of("mangrove_log", "mangrove_leaves", "mangrove_roots");
             case "bamboo" -> Set.of("bamboo", "bamboo_block");
-            case "stone", "stone-craft" -> Set.of("stone", "cobblestone", "andesite", "diorite", "granite");
-            case "craft-basic" -> Set.of("oak_log", "oak_leaves", "birch_log", "birch_leaves");
+            case "stone" -> Set.of("stone", "cobblestone", "andesite", "diorite", "granite");
             case "geology" -> Set.of("stone", "granite", "diorite", "andesite", "tuff", "clay", "mud");
             case "iron" -> Set.of("iron_ore", "deepslate_iron_ore");
             case "copper" -> Set.of("copper_ore", "deepslate_copper_ore");
